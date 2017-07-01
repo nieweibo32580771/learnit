7befc26f471babaa30dfa38a94c2ff047583e622
neiweibo <1165679601@qq.com>
2017-07-01 15:14:38 +0800

    index.php
    Signed-off-by: neiweibo <1165679601@qq.com>

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+$string = 'welcomet123你在哪'; 
+if (preg_match_all( "/^[\x4e00-\x9fa5_a-zA-Z0-9]+$/", '123456niweibonie聂伟博', $m )) { 
+echo "好"; 
+echo "<pre>";
+print_r($m); 
+} else { 
+echo "<font color='red'>正则表达式{$pattern}和字符串{$string}匹配失败</font>"; 
+}
+echo time();echo "<pre>";
+$a = 1497222222;
+$b = 1497022255;
+$c = 1497448451;
+echo date('Y-m-d',$a);//2017-06-12
+echo date('Y-m-d',$b);//2017-06-09
+echo date('Y-m-d',$b);//2017-06-14
+?>
